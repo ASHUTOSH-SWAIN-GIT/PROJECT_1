4132a7aac19ad345b875f5d38149b3397d3df20d
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-02-13 07:03:41 +0530

    feat: Add Inventory model with schema for name, quantity, and price

---
+++ backend/models/InventoryModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const InventorySchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+
+})
